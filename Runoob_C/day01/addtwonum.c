ca9a288aded4b5ecaeec5f81c5bcd823011abdb3
PuZhengKang <2195207622@qq.com>
2024-05-13 15:49:10 +0800

    修改了注释

--- Runoob_C/day01/addtwonum.c
+++ Runoob_C/day01/addtwonum.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-// 声明两个外部变量
+//声明两个外部变量
 extern int x ;
 extern int y ;
 int addtwonum()
---
+++ Runoob_C/day02/define_use.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#define LENGTH 10
+#define WIDTH 5
+#define NEWLINE '\n'
+
+int main() {
+    int area;
+    area = LENGTH * WIDTH;        // 计算面积
+    printf("The area of the rectangle is %d", area);
+    printf("%c",NEWLINE);//换行
+    return 0;
+}
