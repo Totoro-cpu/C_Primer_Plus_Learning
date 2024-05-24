2700af74f173d8c509b486f26f757e53fba0176e
PuZhengKang <2195207622@qq.com>
2024-05-24 17:25:15 +0800

    加了一个指针赋值为空，输出为地址值

--- Runoob_C/day03/pointer_use.c
+++ Runoob_C/day03/pointer_use.c
@@ -2,12 +2,14 @@
 #include <stdlib.h>
 int main()
 {
+    int *q = NULL;
     int num = 66;
     int *p;
     p = &num; // 取地址
     printf("num的地址是：%p\n", &num);
     printf("p存储的地址是：%p\n", p);
     printf("num的值是：%d\n", num);
-    printf("p指向的值是：%d\n", *p); // 取值
+    printf("p指向的值是：%d\n", *p);
+    printf("q的值是：%p\n", q); // 取值
     return 0;
 }
