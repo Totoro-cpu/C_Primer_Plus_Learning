e8ec6f8dd15b2bab82ab5843cfd1773665b39eaa
PuZhengKang <2195207622@qq.com>
2024-05-28 15:41:34 +0800

    修改了注释

--- Runoob_C/day03/pointer_arry.c
+++ Runoob_C/day03/pointer_arry.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
 int main()
 {
+    // 声明数组
     int arr[5] = {1, 2, 3, 4, 5};
+    // 声明指针数组
     int *ptr[5];
-
+    // 给指针数组赋值
     for (int i = 0; i < 5; i++)
     {
         ptr[i] = &arr[i];
     }
+    // 输出指针数组的值
     for (int i = 0; i < 5; i++)
     {
         printf("地址为%p ", ptr[i]);
