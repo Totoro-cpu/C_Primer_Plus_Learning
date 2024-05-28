427d55899e9fdef12afbe551554ef25b91335b5a
PuZhengKang <2195207622@qq.com>
2024-05-28 15:15:09 +0800

    修改了注释以及输出的字符串

--- Runoob_C/day03/pointer_cal_com.c
+++ Runoob_C/day03/pointer_cal_com.c
@@ -5,9 +5,9 @@ int main()
     int a[MAX_SIZE] = {10, 20, 30};
     int *ptr;
     ptr = a;
-    while (ptr < &a[MAX_SIZE])
+    while (ptr < &a[MAX_SIZE])//判断指针是否小于数组的最后一个元素
     {
-        printf("存储地址为 %p", ptr);
+        printf("数组中存储地址为 %p", ptr);
         printf("的值是 %d\n", *ptr);
         ptr++;
     }
