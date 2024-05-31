e9eb8490830619a593b41a813711dbdda51d7352
PuZhengKang <2195207622@qq.com>
2024-05-31 10:44:21 +0800

    编写位域代码

---
+++ Runoob_C/day05/bit-field_use.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+// 定义普通结构
+struct
+{
+    int a1;
+    int b2;
+} num1;
+// 定义位域结构
+struct
+{
+    int a2 : 1;
+    int b2 : 1;
+} num2;
+int main()
+{
+    // 打印两个结构所占用的内存大小
+    printf("the size of num1 is %d\n", sizeof(num1));
+    printf("the size of num2 is %d\n", sizeof(num2));
+    return 0;
+}
